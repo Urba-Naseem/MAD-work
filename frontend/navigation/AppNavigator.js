91ba2f0e48d592be1bd51cf47131cbb837d65a0e
Urba-Naseem <urbanaseem2274@gmail.com>
2026-04-22 19:25:13 +0500

    Notes/Movies

--- frontend/navigation/AppNavigator.js
+++ frontend/navigation/AppNavigator.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { GestureHandlerRootView } from 'react-native-gesture-handler'; // Import the wrapper
 import { useAuth } from '../context/AuthContext';
 import AuthNavigator from './AuthNavigator';
 import MainTabNavigator from './MainTabNavigator';
 import Loader from '../components/Loader';
-import { COLORS } from '../config/theme'; // Import your theme for background
+import { COLORS } from '../config/theme'; 
 
 const AppNavigator = () => {
   const { user, loading } = useAuth();
@@ -13,11 +12,9 @@ const AppNavigator = () => {
   if (loading) return <Loader />;
 
   return (
-    <GestureHandlerRootView style={{ flex: 1, backgroundColor: COLORS.background }}>
-      <NavigationContainer>
-        {user ? <MainTabNavigator /> : <AuthNavigator />}
-      </NavigationContainer>
-    </GestureHandlerRootView>
+    <NavigationContainer>
+      {user ? <MainTabNavigator /> : <AuthNavigator />}
+    </NavigationContainer>
   );
 };
 
